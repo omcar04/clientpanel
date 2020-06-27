6cd28c0affa8369cfc97c992f9e12c7293ddc7cb
Omkar Parab <omcar04@gmail.com>
2020-06-27 20:23:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppNavbar from "./components/layout/AppNavbar";
+import Dashboard from "./components/layout/Dashboard";
+import AddClient from "./components/clients/AddClient";
+import EditClient from "./components/clients/EditClient";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
+import ClientDetails from "./components/clients/ClientDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <AppNavbar></AppNavbar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard}></Route>
+              <Route exact path="/client/add" component={AddClient}></Route>
+              <Route
+                exact
+                path="/client/edit/:id"
+                component={EditClient}
+              ></Route>
+              <Route exact path="/client/:id" component={ClientDetails}></Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,55 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
+import { reduxFirestore, firestoreReducer } from "redux-firestore";
+//Reducers
+// @todo
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZmVPjNknXAioBHW0TegqQTtbFNKDneg4",
+  authDomain: "reactclientpanel-e9307.firebaseapp.com",
+  databaseURL: "https://reactclientpanel-e9307.firebaseio.com",
+  projectId: "reactclientpanel-e9307",
+  storageBucket: "reactclientpanel-e9307.appspot.com",
+  messagingSenderId: "794826154749",
+};
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+
+// Init firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Init firestore
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
+
+//Add reactReduxFirebase enhancer
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
+)(createStore);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer, // <- needed if using firestore
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
